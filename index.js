3d0fad6aa0fc6d80858c8310d3dd8c6c79909f90
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2020-10-06 14:50:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 
 
+var http = require("http");
+
+//create a server object:
+
+http
+
+  .createServer(function(req, res) {
+
+    res.write("create by infinitrocyber"); //write a response to the client
+
+    res.end(); //end the response
+
+  })
+
+  .listen(8080); //the server object listens on port 8080
+
 
 
 const { Telegraf } = require('telegraf')
